542d52b7dcaf9cce2902adfbf1972e005449ec46
Arati Kushwaha <arati.kushwaha.work@gmail.com>
2026-02-05 23:26:11 +0530

    Add files via upload
    Uploaded Todo App with HTML, CSS and JavaScript

---
+++ script.js
@@ -0,0 +1,25 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value.trim();
+
+  if (taskText === "") {
+    alert("Please enter task!");
+    return;
+  }
+  
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  const btn = document.createElement("button");
+  btn.textContent = "Delete";
+  btn.onclick = function () {
+            li.remove();
+  };
+
+  li.appendChild(btn);
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+  }
+ 
